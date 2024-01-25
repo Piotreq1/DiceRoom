e19ca02a76bca187dbeed72b986431a98c9a892c
PiotrSitarek1 <235969@edu.p.lodz.pl>
2024-01-25 20:02:17 +0100

    Meeting manager implementation, connection with firebase

---
+++ app/src/main/java/com/example/diceroom/managers/MeetingManager.kt
@@ -0,0 +1,123 @@
+package com.example.diceroom.managers
+
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+
+data class MeetingModel(
+    val title: String = "",
+    val game: String = "",
+    val location: String = "",
+    val creationDate: String = "",
+    val startDate: String = "",
+    val ownerId: String = "",
+    val level: String = "",
+    val description: String = "",
+    val image: String = "",
+    val participants: List<String>? = null
+)
+
+
+class MeetingManager {
+
+    private val meetingRef: DatabaseReference =
+        FirebaseDatabase.getInstance().reference.child("meetings")
+
+    fun addMeeting(meetingModel: MeetingModel, onComplete: (Boolean, String?) -> Unit) {
+        meetingRef.setValue(meetingModel).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                onComplete(true, null)
+            } else {
+                onComplete(false, task.exception?.message)
+            }
+        }
+    }
+
+    fun updateMeeting(
+        meetingId: String, updatedFields: Map<String, Any>, onComplete: (Boolean, String?) -> Unit
+    ) {
+        meetingRef.child(meetingId).updateChildren(updatedFields).addOnCompleteListener {
+            if (it.isSuccessful) {
+                onComplete(true, null)
+            } else {
+                onComplete(false, it.exception?.message)
+            }
+        }
+    }
+
+    fun getMeetingById(meetingId: String, onComplete: (MeetingModel?) -> Unit) {
+        meetingRef.child(meetingId).get().addOnSuccessListener {
+            onComplete(it.getValue(MeetingModel::class.java))
+        }.addOnFailureListener {
+            onComplete(null)
+        }
+    }
+
+    fun getParticipants(meetingId: String, onComplete: (List<String>?) -> Unit) {
+        val meetingParticipantsRef = meetingRef.child(meetingId).child("participants")
+
+        meetingParticipantsRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                onComplete(snapshot.value as? List<String>)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                onComplete(null)
+            }
+        })
+    }
+
+
+    fun addParticipant(meetingId: String, userId: String, onComplete: (Boolean, String?) -> Unit) {
+        val meetingParticipantsRef = meetingRef.child(meetingId).child("participants")
+
+        meetingParticipantsRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val currentParticipants: MutableList<String> = if (snapshot.exists()) {
+                    snapshot.value as? MutableList<String> ?: mutableListOf()
+                } else {
+                    mutableListOf()
+                }
+
+                currentParticipants.add(userId)
+                meetingParticipantsRef.setValue(currentParticipants).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        onComplete(true, null)
+                    } else {
+                        onComplete(false, it.exception?.message)
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                onComplete(false, error.message)
+            }
+        })
+    }
+
+    fun deleteParticipant(
+        meetingId: String, userId: String, onComplete: (Boolean, String?) -> Unit
+    ) {
+        val meetingParticipantsRef = meetingRef.child(meetingId).child("participants")
+
+        meetingParticipantsRef.orderByValue().equalTo(userId)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (childSnapshot in snapshot.children) {
+                        childSnapshot.ref.removeValue()
+                        onComplete(true, null)
+                        return
+                    }
+                    onComplete(false, "User not found in participants")
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    onComplete(false, error.message)
+                }
+            })
+    }
+}
--- app/src/main/java/com/example/diceroom/meetings/MeetingFragment.kt
+++ app/src/main/java/com/example/diceroom/meetings/MeetingFragment.kt
@@ -5,12 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.diceroom.R
+import com.example.diceroom.databinding.FragmentMeetingBinding
+import com.example.diceroom.managers.MeetingManager
+import com.example.diceroom.managers.MeetingModel
+import com.example.diceroom.utils.Utils
 
 class MeetingFragment : Fragment() {
+    private val meetingManager = MeetingManager()
+    private val utils = Utils()
+    private lateinit var bind: FragmentMeetingBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_meeting, container, false)
+        bind = FragmentMeetingBinding.inflate(layoutInflater)
+
+        // TODO: ITS TEST to be deleted later
+        val exampleMeeting = MeetingModel(
+            title = "Team Meeting",
+            game = "Chess",
+            location = "Conference Room",
+            creationDate = "2024-01-25",
+            startDate = "2024-01-30",
+            ownerId = "123456789",
+            level = "Intermediate",
+            description = "Discuss strategy for upcoming chess tournament",
+            image = "meeting_image.jpg",
+            participants = listOf("123456789")
+        )
+        bind.addMeetingButton.setOnClickListener{
+            meetingManager.addMeeting(exampleMeeting) { isSuccess, message ->
+                utils.handleFirebaseResult(
+                    isSuccess,
+                    message,
+                    requireContext(),
+                    "Meeting successfully created",
+                    "Meeting creation failure"
+                )
+            }
+        }
+
+
+        return bind.root
     }
 }
